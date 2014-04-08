5d0b734d1b79b783833679f0a73581394a7a32ce
Scott Fay <scott.a.fay@gmail.com>
2014-04-07 23:46:49 -0700

    more portable, saves lists of genes in each cluster

--- diff_expression/clustering_and_heatmap.R
+++ diff_expression/clustering_and_heatmap.R
@@ -17,10 +17,9 @@ library(Biobase)
 #####
 #
 # User-set paramaters
+# TODO: make these command line parameters
 #
 #####
-
-outfile_prefix <- "outfile"
 
 # set working directory
 setwd("/Users/Shared/BigCB_Insect_Project/Dicosmo_project/Dicosmo_DGE")
@@ -40,29 +39,35 @@ k = 16
 #
 #####
 
-data <- data.matrix(all_fpkm[(rownames(all_fpkm) %in% targetList$transcript_id),]) # make matrix of fpkm values using only those transcripts found in targetList
-
-## generate correlation matrix
+# make matrix of fpkm values using only those transcripts found in targetList
+data <- data.matrix(all_fpkm[(rownames(all_fpkm) %in% targetList$transcript_id),])
+# generate correlation matrix
 cr = cor(data, method='spearman')
-## log2 transform, mean center rows
+# log2 transform, mean center rows
 data = log2(data+1)
 centered_data = t(scale(t(data), scale=F)) # center rows, mean substracted
-hc_genes = agnes(centered_data, diss=FALSE, metric="euclidean") # cluster genes
-hc_samples = hclust(as.dist(1-cor(centered_data, method="spearman")), method="complete") # cluster conditions
+# cluster genes
+hc_genes = agnes(centered_data, diss=FALSE, metric="euclidean") 
+# cluster conditions
+hc_samples = hclust(as.dist(1-cor(centered_data, method="spearman")), method="complete") 
 myheatcol = redgreen(75)
+# partition gene clusters based on "k"
 gene_partition_assignments <- cutree(as.hclust(hc_genes), k=k)
 partition_colors = rainbow(length(unique(gene_partition_assignments)), start=0.4, end=0.95)
 gene_colors = partition_colors[gene_partition_assignments]
-#postscript(file=paste(out_dir,"diff_expr_matrix_file.heatmap.eps",sep=""), horizontal=FALSE, width=18, height=8, paper="special")
+
+# draw heatmap
+postscript(file=paste(out_dir,"diff_expr_matrix_file.heatmap.eps",sep=""), horizontal=FALSE, width=18, height=8, paper="special")
 heatmap.2(centered_data, dendrogram="both", Rowv=as.dendrogram(hc_genes), Colv=as.dendrogram(hc_samples), col=myheatcol, RowSideColors=gene_colors, scale="none", density.info="none", trace="none", key=TRUE, keysize=1.2, cexCol=2.5, margins=c(15,15), lhei=c(0.3,2), lwid=c(2.5,4))
-#dev.off()
+dev.off()
 
 # prep for clustering
 max_cluster_count = max(gene_partition_assignments)
 gene_names = rownames(data)
 num_cols = length(data[1,])
 
-# partition data into subclusters, print plots
+# partition data into subclusters, save plots of each cluster as .pdf files 
+#   with traces for each transcript's normalized expression levels across treatments
 for (i in 1:max_cluster_count) {
   partition_i = (gene_partition_assignments == i)
   partition_data = centered_data[partition_i,]
@@ -75,6 +80,11 @@ for (i in 1:max_cluster_count) {
   assign(paste("subcluster_", i, sep=""), as.data.frame(partition_data))
   cluster_data <- as.data.frame(partition_data)
   cluster_fpkm_means <- rbind(data.frame(temp='15C', mean_fpkm=apply(cluster_data[,1:5], 1, mean), gene_id=rownames(cluster_data)), data.frame(temp='20C', mean_fpkm=apply(cluster_data[,6:10], 1, mean), gene_id=rownames(cluster_data)), data.frame(temp='25C', mean_fpkm=apply(cluster_data[,11:15], 1, mean), gene_id=rownames(cluster_data)), data.frame(temp='30C', mean_fpkm=apply(cluster_data[,16:20], 1, mean), gene_id=rownames(cluster_data)))
-  #  p <- ggplot(cluster_fpkm_means, aes(x=temp, y=mean_fpkm, group=gene_id))
-  print(ggplot(cluster_fpkm_means, aes(x=temp, y=mean_fpkm, group=gene_id)) + geom_line(color=partition_colors[i],size=1, alpha=0.2) + geom_point(size = 3, alpha = 0.2) + theme_bw(base_size = 24) + ggtitle(NULL)) # + ggtitle(paste("Cluster_", i, sep="")) + ylab("median-centered log2(FPKM+1)") ))
+  
+  p <- ggplot(cluster_fpkm_means, aes(x=temp, y=mean_fpkm, group=gene_id)) + geom_line(color=partition_colors[i],size=1, alpha=0.2) + geom_point(size = 3, alpha = 0.2) + theme_bw(base_size = 24) + ggtitle(NULL) # + ggtitle(paste("Cluster_", i, sep="")) + ylab("median-centered log2(FPKM+1)") ))
+  ggsave(filename=paste("cluster_", i, ".pdf", sep=""), plot=p)
 }
+
+# print file of subclusters, for downstream applications
+transcript_clusters <- data.frame(transcript_id = names(gene_partition_assignments), cluster = gene_partition_assignments)
+write.table(transcript_clusters, file = "transcript_clusters.txt", row.names = FALSE, quote = FALSE, sep = "\t")
