9c27c1509d53a5900a48ed8b6aa8d1f28d6abe92
Scott Fay <scott.a.fay@gmail.com>
2014-04-10 15:15:35 -0700

    updated genewise_analysis.R and prep_clustering.R

--- diff_expression/genewise_analysis.R
+++ diff_expression/genewise_analysis.R
@@ -36,29 +36,78 @@ transcripts <- read.delim("/Users/scottfay/annotation/Calineuria/trinotate_annot
 #
 ##########
 
-####
-# Function: get_gene_data
-# examine expression of an individual gene based on transcript_id
-####
 
-get_gene_data <- function(gene, fpkm) {
-  mean_15 <- apply(fpkm[gene,1:5], 1, mean)
-  mean_20 <- apply(fpkm[gene,6:10], 1, mean)
-  mean_25 <- apply(fpkm[gene,11:15], 1, mean)
-  mean_30 <- apply(fpkm[gene,16:20], 1, mean)
-  sd_15 <- apply(fpkm[gene,1:5], 1, sd)
-  sd_20 <- apply(fpkm[gene,6:10], 1, sd)
-  sd_25 <- apply(fpkm[gene,11:15], 1, sd)
-  sd_30 <- apply(fpkm[gene,16:20], 1, sd)
-  fpkm_mean <- c(mean_15, mean_20, mean_25, mean_30)
-  SD <- c(sd_15, sd_20, sd_25, sd_30)
-  temp <- c("15C", "20C", "25C", "30C")
-  out_frame <- data.frame(fpkm_mean, SD, temp)
+
+#####
+#
+# JS plyr version
+#
+#####
+pH # use as.factor from group file, or make this as a factor
+temp # use as.factor from group file, or make this as a factor
+
+library(plyr)
+library(ggplot2)
+
+gene="comp0_c0_seq1"
+
+get_gene_data <- function(gene, all_fpkm) {
+  gene_data <- data.frame(fpkm=as.numeric(all_fpkm[gene,]), pH=pH, Temperature=temp)
+  fpkm_mean<-ddply(gene_data,.(pH,Temperature), function(d) mean(d$fpkm)) # if group file has columns called pH and Temperature
+  SD<-ddply(gene_data,.(pH,Temperature), function(d) sd(d$fpkm)) # if group file has columns called pH and Temperature
+  names(fpkm_mean)=c("pH","Temperature","Mean") # rename
+  names(SD)=c("pH","Temperature","SD") # rename
+  out_frame=merge(fpkm_mean,SD) #make one data frame
+  out_frame=out_frame[,c(3,4,1,2)] # reorder columns
   return(out_frame)
 }
 
-# test getting fpkm data for a specific gene
-#gene_data <- get_gene_data("comp63351_c0_seq3", all_fpkm)
+gene_data <- get_gene_data("comp0_c0_seq1", all_fpkm)  #the gene in all_fpkm for which data will be returned
+
+
+
+
+
+####
+# Function: get_gene_data
+# examine expression of an individual gene based on transcript_id
+# Scott's method
+####
+# 
+# 
+# 
+# get_gene_data <- function(gene, fpkm) {
+#   A_mean <- apply(fpkm[gene,1:5], 1, mean)
+#   B_mean <- apply(fpkm[gene,6:10], 1, mean)
+#   C_mean <- apply(fpkm[gene,11:15], 1, mean)
+#   D_mean <- apply(fpkm[gene,16:20], 1, mean)
+#   E_mean <- apply(fpkm[gene,21:25], 1, mean)
+#   F_mean <- apply(fpkm[gene,26:30], 1, mean)
+#   G_mean <- apply(fpkm[gene,31:35], 1, mean)
+#   H_mean <- apply(fpkm[gene,36:40], 1, mean)
+#   J_mean <- apply(fpkm[gene,41:45], 1, mean)
+#   A_sd <- apply(fpkm[gene,1:5], 1, sd)
+#   B_sd <- apply(fpkm[gene,6:10], 1, sd)
+#   C_sd <- apply(fpkm[gene,11:15], 1, sd)
+#   D_sd <- apply(fpkm[gene,16:20], 1, sd)
+#   E_sd <- apply(fpkm[gene,21:25], 1, sd)
+#   F_sd <- apply(fpkm[gene,26:30], 1, sd)
+#   G_sd <- apply(fpkm[gene,31:35], 1, sd)
+#   H_sd <- apply(fpkm[gene,36:40], 1, sd)
+#   J_sd <- apply(fpkm[gene,41:45], 1, sd)
+#   fpkm_mean <- c(A_mean, B_mean, C_mean, D_mean, E_mean, F_mean, G_mean, H_mean, J_mean)
+#   SD <- c(A_sd, B_sd, C_sd, D_sd, E_sd, F_sd, G_sd, H_sd, J_sd)
+#   names <- c("A", "B", "C", "D", "E", "F", "G", "H", "J")
+#   pH_grp <- c("amb", "low", "amb", "low", "mid", "amb", "mid", "low", "mid")
+#   temp_grp <- c("13C", "11C", "11C", "14C", "13C", "14C", "14C", "13C", "11C")
+#   out_frame <- data.frame(fpkm_mean, SD, pH_grp, temp_grp, names)
+#   out_frame$pH_grp <- factor( out_frame$pH_grp, levels = c( "amb", "mid", "low" ) )
+#   return(out_frame)
+# }
+
+# get fpkm data for a specific gene
+#gene_data <- get_gene_data("comp106087_c0_seq1", all_fpkm)
+
 
 #####
 # Function: get_annot
@@ -68,7 +117,7 @@ get_gene_data <- function(gene, fpkm) {
 get_annot <- function(gene_in, annotation) {
   L <- annotation$transcript_id == gene_in
   cat("transcript: ", as.vector(annotation[L,]$transcript_id), "\n")
-  cat("top blast hit: ", as.vector(annotation[L,]$Top_BLASTX_hit), "\n")
+  cat("top blast hit: ", as.vector(annotation[L,]$TopBlastHit), "\n")
   cat("gene ontology: ", as.vector(annotation[L,]$gene_ontology), "\n")
   cat("PFam: ", as.vector(annotation[L,]$Pfam), "\n")
   cat("Signal Peptide: ", as.vector(annotation[L,]$SignalP), "\n")
@@ -77,21 +126,35 @@ get_annot <- function(gene_in, annotation) {
 }
 
 # test getting annotation data
-#get_annot("comp61673_c0_seq1", transcripts)
+#get_annot("comp99839_c0_seq1", transcripts)
 
 #####
 # Function: plot_genewise_fpkm
 # plots fpkm for the specified gene
 #####
 
-plot_genewise_fpkm <- function(gene_in, plot_title, add_title) {
-  k <- ggplot(gene_in, aes( x = temp, y = fpkm_mean, ymax = fpkm_mean + SD, ymin = fpkm_mean - SD ) )
-  k + geom_point( size = 3 ) + geom_errorbar(width = 0.4) + theme_bw() + theme(plot.title = element_text(size = rel(0.9)), axis.title = element_text(size = rel(0.8))) + labs(title=paste(plot_title, "\n", add_title), x = "Temperature", y = "mean FPKM")
-  ggsave(filename = paste(out_dir, plot_title, "_", add_title, "_genewise_FPKM", ".pdf", sep =""), width = 2, height = 2.5)
+# plot_genewise_fpkm <- function(gene_in, plot_title, add_title) {
+#   k <- ggplot(gene_in, aes( x = temp_grp, y = fpkm_mean, ymax = fpkm_mean + SD, ymin = fpkm_mean - SD, fill = temp_grp ))
+#   k + geom_bar(stat="identity") + facet_grid( . ~ pH_grp ) + geom_errorbar( width = 0.3) + scale_fill_brewer(type="qual", palette="Paired") + labs(title=paste(plot_title, add_title))
+#   k + geom_bar(stat="identity") + facet_grid( . ~ pH_grp ) + geom_errorbar( width = 0.3) + scale_fill_brewer(type="qual", palette="Paired") + labs(title=paste(plot_title, add_title))
+#   ggsave(filename = paste(out_dir, plot_title, "_", add_title, "_genewise_FPKM", ".pdf", sep =""), width = 6, height = 6)
+#   }
+
+# test plotting fpkm data
+#plot_genewise_fpkm(gene_data, "titleA", "titleB")
+
+#####
+# function: plot_interaction
+# makes an interaction plot for a given gene
+#####
+
+plot_interaction <- function(gene_data, plot_title, add_title) {
+  h <- ggplot(gene_data, aes(x = pH, y = Mean, ymax = Mean + SD, ymin = Mean - SD, color=Temperature))
+  h + geom_errorbar(color="black", width=0.1) + geom_point(aes(shape=Temperature), color="black",size = 6) + geom_line(aes(group=Temperature), size=0.5, linetype=1) + labs(title=paste(plot_title, add_title), x = "pH treatment", y = "mean FPKM") + scale_color_manual(values=c("#3399FF", "#66CC00", "#CC0066"))
+  ggsave(filename = paste(out_dir, plot_title, "_", add_title, "_interactionPlot_FPKM", ".pdf", sep =""), width = 6, height = 6)
 }
 
-#plot_genewise_fpkm(gene_data, "comp63351_c0_seq3", " expression")
-
+#plot_interaction(gene_data,"TITLEA","TITLEB")
 #####
 # function: GOI_data
 # master function to get data and plots from Gene Of Interest
@@ -103,9 +166,24 @@ plot_genewise_fpkm <- function(gene_in, plot_title, add_title) {
 GOI_data <- function(gene_name, annotation, fpkm_table, graph_title) {
   gene_data <- get_gene_data(gene_name, fpkm_table)
   get_annot(gene_name, annotation)
-  plot_genewise_fpkm(gene_data, gene_name, graph_title)
+  #plot_genewise_fpkm(gene_data, gene_name, graph_title)
+  plot_interaction(gene_data, gene_name, graph_title)
 }
 
 # generate genewise plots based on a gene of interest
-GOI_data("comp55133_c0_seq1", transcripts, all_fpkm, "Zinc Finger")
+GOI_data("comp100107_c0_seq1", transcripts, all_fpkm, "Carbonic Anhydrase")
+
+###
+#
+#  Example: read in a list of genes of interest, and make plots
+#
+###
+
+desired_graphs = read.csv(file.choose())  #a text file with two columns, the geneID (gene_name) and a descriptor (graph_title)
+
+for (i in 1:nrow(desired_graphs)) {
+  GOI_data(desired_graphs[i,1], transcripts, all_fpkm, desired_graphs[i,2])
+}
+
+
 
--- diff_expression/prep_clustering.R
+++ diff_expression/prep_clustering.R
@@ -1,7 +1,5 @@
 #!/usr/bin/Rscript
 
-library(optparse)
-
 #####
 #
 # Using data from "import_express_data.R,"
@@ -45,7 +43,63 @@ write.csv(centered_data, "centered_data.csv")
 
 # this file ready to cluster with Cluster 3.0
 
+DEGlist_lrt_inter_0
+
+# read in the groupings again, this time with the columns as characters (not factors)
+names <- read.delim("../RKC5_groups.txt", colClasses = c(rep("character",4)))
+names = names[-9,]
+
+# function to concatenate columns for names
+concat_name <- function(x){
+  paste(x[1],x[2],x[3],x[4],sep="_")
+}
+
+# concatenate column names from groupings to make the column names more informative
+new_column_names <- apply(names, 1, concat_name)
+
+transcripts$CloneID <- transcripts$transcript_id
+head(transcripts)
+
+#####
+#
+# function to write cluster file
+#   arguments:
+#     all_fpkm = all_fpkm, a dataframe of fpkm values
+#     unique_DE_transcripts, a list of differentially expressed genes
+#     outfile = path and name of output file
+#     cnames = vector of new column names
+#
+#####
 
 
-
-
+write.cluster.file <- function(all_fpkm, unique_DE_transcripts, outfile, cnames) {
+  # make matrix of fpkm values using only those transcripts found in all targetList
+  FPKM_all <- data.matrix(all_fpkm[(rownames(all_fpkm) %in% unique_DE_transcripts),])
+  # log2 transform, mean center rows
+  FPKM_all = log2(FPKM_all+1)
+  centered_data = t(scale(t(FPKM_all), scale=F)) # center rows, mean substracted
+  # make data frame of centered_data matrix
+  centeredDF <- as.data.frame(centered_data)
+  # concatenate column names from groupings to make the column names more informative
+  colnames(centeredDF) <- cnames
+  # add CloneID vector to centeredDF
+  centeredDF$CloneID <- rownames(centered_data)
+  # get vector of cloneID
+  cloneIDs <- centeredDF$CloneID
+  ### generate a new vector of topBlastHits for those cloneIDs
+  # generate a subset data frame of the matches to transcript_id in the annotation dataframe; this makes the search in the loop below much faster
+  annot_matches <- transcripts[(transcripts$transcript_id %in% cloneIDs),]
+  # initialize an empty vector
+  blasthits <- c()
+  # iterate over cloneID in centeredDF, appending each topBlastHit to the blasthits vector
+  for (cloneID in centeredDF$CloneID) {
+    match <- annot_matches[annot_matches$transcript_id %in% cloneID,]
+    blasthits <- c(blasthits, match$TopBlastHit[1])
+  }
+  # add blasthits vector to centeredDF "NAME" column
+  centeredDF$NAME <- blasthits
+  # reorder columns
+  centeredDF <- centeredDF[,c(ncol(centeredDF)-1,ncol(centeredDF),1:(ncol(centeredDF)-2))] # more generalized form
+  # save centeredDF 
+  write.table(centeredDF, file=paste(out_dir, "centered_data_all_comparison_DEGs.txt", sep=""), quote=FALSE, row.names=FALSE, sep="\t")
+}
